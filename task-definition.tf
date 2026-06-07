52823b377e6759ed54617c6ce5d4f061b05fc8bc
kavya <kavyak4719@gmail.com>
2026-06-07 17:40:25 +0530

    Day 4 completed - CloudWatch logging and ECS autoscaling

--- task-definition.tf
+++ task-definition.tf
@@ -21,6 +21,16 @@ resource "aws_ecs_task_definition" "app" {
           hostPort      = 3000
         }
       ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.ecs.name
+          awslogs-region        = "ap-south-1"
+          awslogs-stream-prefix = "ecs"
+        }
+      }
     }
   ])
 }
